c8d0918d64c0068874521e52e66690de42846d16
1000pilar <nunua7a8@gmailcom>
2017-05-16 16:06:36 +0700

    nyoba ubah router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  models.Employee.findAll()
+    .then(employee => {
+      res.render('index', {
+        employees : employee
+      })
+    })
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../models')
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  models.Employee.findAll()
-    .then(employees => {
-      res.render('index', {
-        employees : employee
-      })
-    })
+  res.send("haha");
 });
 
 
