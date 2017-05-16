0fa89f3a80af6d57f668152deb893dc8b7c16d5e
1000pilar <nunua7a8@gmailcom>
2017-05-16 14:45:59 +0700

    first touch

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../models')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Heroku for the first Time');
-});
-
-router.get('/list', function(req, res, next) {
-  res.send('Heroku for the first Time');
+  res.send('respond with a resource');
 });
 
 module.exports = router;
