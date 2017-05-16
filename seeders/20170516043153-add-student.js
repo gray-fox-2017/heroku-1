66212736ee9cd2af047e3ecf188028285dbae7dc
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-05-16 11:48:19 +0700

    creating first releases

---
+++ seeders/20170516043153-add-student.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Students', [{
+      first_name: 'Deri',
+      last_name: 'Kurniawan',
+      createdAt: new Date().toISOString,
+      updatedAt: new Date().toISOString
+    }], [{
+      first_name: 'Nugraha',
+      last_name: 'Nugraha',
+      createdAt: new Date().toISOString,
+      updatedAt: new Date().toISOString
+    }], [{
+      first_name: 'Ambo',
+      last_name: 'Dalle',
+      createdAt: new Date().toISOString,
+      updatedAt: new Date().toISOString
+    }], [{
+      first_name: 'Widya',
+      last_name: 'Kandau',
+      createdAt: new Date().toISOString,
+      updatedAt: new Date().toISOString
+    }], {})
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+  return queryInterface.bulkDelete('Students', null, {});
+};
