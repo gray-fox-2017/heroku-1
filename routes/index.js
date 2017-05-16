9f92c5a97fff5a174f7a0d2e4abedbad5311c0fd
1000pilar <nunua7a8@gmailcom>
2017-05-16 16:18:01 +0700

    akhirnya

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,12 @@ var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // models.Employee.findAll()
-  //   .then(employee => {
-  //     res.render('index', {
-  //       employees : employee
-  //     })
-  //   }
-  res.render('index')
+  models.Employee.findAll()
+    .then(employee => {
+      res.render('index', {
+        employees : employee
+      })
+    }
 });
 
 module.exports = router;
