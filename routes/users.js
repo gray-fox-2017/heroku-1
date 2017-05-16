99be59fdc3b5aba3304a409c7ec84022d855d5fd
1000pilar <nunua7a8@gmailcom>
2017-05-16 16:04:23 +0700

    nyoba ubah router

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,8 @@ var models = require('../models')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  models.Employee.findAll({})
-    .then (function(employees){
+  models.Employee.findAll()
+    .then(employees => {
       res.render('index', {
         employees : employee
       })
